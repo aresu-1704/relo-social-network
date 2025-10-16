a1dfc3b280ccf4d3f5a02cbd8e147b156e8bf8b8
aresu-1704 <aresu.work1704@gmail.com>
2025-10-16 13:57:52 +0700

    Clean kiến trúc

--- api/setup.bat
+++ api/setup.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Creating virtual environment...
-python -m venv venv
+python -m venv .venv
 
 echo Installing dependencies...
-call venv\Scripts\pip.exe install -r requirements.txt
+call .venv\Scripts\pip.exe install -r requirements.txt
 
 echo Setup complete.
 pause
